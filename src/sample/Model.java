4320c603db6ae146ae1bf7c7e39559e25ff3cfa9
zulwik <Pom@r@ncz@1986>
2018-02-01 21:43:06 +0100

    11

---
+++ src/sample/Model.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class Model
+{
+
+    public long calculation(long a, long b, String operator)
+    {
+        switch (operator)
+        {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                if (b == 0) return 0;
+                return a / b;
+        }
+        System.out.println("Niedozwolony operator"+ operator);
+        return 0;
+    }
+}
